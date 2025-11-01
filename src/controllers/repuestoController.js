4c10be7af2ccc94d9203c279727b99aa2fc9105f
Jonathan Netto <jonanetto@Jonathans-MacBook-Air.local>
2025-11-01 12:36:21 -0300

    Se modifica función getMarcaRepuesto

--- src/controllers/repuestoController.js
+++ src/controllers/repuestoController.js
@@ -1,4 +1,5 @@
 // src/controllers/repuestoController.js
+const Orden = require('../models/Orden');
 const Repuesto = require('../models/Repuesto');
 
 exports.getRepuestos = async (req, res) => {
@@ -50,31 +51,50 @@ exports.deleteRepuesto = async (req, res) => {
   }
 };
 
+//TODO: Cambiarle el nombre a la función y documentación de swagger, ahora es getRepuestosVendidosPorMarca. Lo que hace es agrupar por marca y sumar la cantidad vendida de las órdenes.
+// Falta punto 6 y editar caso de uso 3 con el marcador correspondientes (consulta comun).
 exports.getMarcaRepuesto = async (req, res) => {
   try {
-    const repuestos = await Repuesto.aggregate([
+    const pipeline = [
+      {
+        $lookup: {
+          from: 'ordenes', 
+          localField: 'nro_serie', 
+          foreignField: 'items.nro_serie', 
+          as: 'ventas_ordenes' 
+        }
+      },
+      { $unwind: '$ventas_ordenes' },
+      { $unwind: '$ventas_ordenes.items' },
+      {
+        $match: {
+          $expr: {
+            $eq: ['$nro_serie', '$ventas_ordenes.items.nro_serie']
+          }
+        }
+      },
       {
         $group: {
-          _id: "$marca",
-          totalRepuestos: { $sum: 1 }, 
-        },
+          _id: '$marca', 
+          totalItemsVendidos: { $sum: '$ventas_ordenes.items.cantidad' } 
+        }
       },
       {
-        $sort: { totalRepuestos: -1 },
-      },
-    ]);
-    res.status(200).json(repuestos);
+        $sort: { totalItemsVendidos: -1 }
+      }
+    ];
+
+    const ventasPorMarca = await Repuesto.aggregate(pipeline);
+    res.status(200).json(ventasPorMarca);
+
   } catch (err) {
+    console.error("Error en getVentasPorMarcaDesdeRepuestos:", err);
     res.status(500).json({ message: err.message });
   }
-
 };
 
 exports.getRepuestosCompatiblesByModelo = async (req, res) => {
   try {
-    // Agrega esta línea temporalmente
-    console.log("Modelo recibido en req.params:", req.params.modelo_coche); 
-    console.log("Modelo recibido en req.params:", req); 
     const repuestos = await Repuesto.find({ modelo_coche: req.params.modelo });
     res.status(200).json(repuestos);
   } catch (err) {
